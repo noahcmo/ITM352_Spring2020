373a8be0def539614551e250d296064afe39cfc5
noahcmo <59903335+noahcmo@users.noreply.github.com>
2020-04-16 23:45:12 -1000

    More progress on Assignment1

--- Okamoto_Noah_Assignment1/server.js
+++ Okamoto_Noah_Assignment1/server.js
@@ -11,55 +11,43 @@ let products_array = require('./shoes.json'); //Got from Assignment 1
 /*Used similar server to Lab13*/
 var express = require('express');
 var app = express();
-var myParser = require("body-parser"); 
+var myParser = require("body-parser");
 var fs = require('fs');
 /*From Lab13*/
 var data = require('./public/product_data.js');
 var products = data.products;
 
 app.all('*', function (request, response, next) {
-    console.log(request.method + ' to ' + request.path);
-    next(); 
+  console.log(request.method + ' to ' + request.path);
+  next();
 });
 
-app.use(myParser.urlencoded({ extended: true })); 
+app.use(myParser.urlencoded({ extended: true })); //gets data from body
 
-app.post("/quantity_form", function (request, response) {
-   let POST = request.body;
-   q = POST["quantity_textbox"]; 
+app.post("/process_form", function (request, response) {
+  let POST = request.body;
 
-    if (typeof POST['quantity_textbox'] != 'undefined') {
+  if (typeof POST['purchase_submit'] != 'undefined') {
+    for (i = 0; i < products.length; i++) {
+
+      q = POST["quantity" + i];
+
       console.log(q);
-    if(isNonNegInt(q, false)) {
-      response.send(`Thank you for purchasing ${q} things!`);
+      if (isNonNegInt(q, false)) {
+        console.log(`Thank you for purchasing ${q} things!`);
       }
-    else{
-         response.send(`${q} is not a quantity! Press the back button and try again.`); 
+      else {
+        console.log(`${q} is not a quantity! Press the back button and try again.`);
       }
-  };
+    }
+  }
+  response.send("Thank you for your purchase! Have a great day!");
 });
+app.get("process_invoice", function (request, response) {
+  if (isNonNegInt(q, false)) {
 
-
+  }
+});
+//If quantity is all good, process invoice. not good, send back to page 
+app.use(express.static('./public'));
 app.listen(8080, () => console.log(`connected to port 8080`));
-
-
-/*Made tax rates up */
-tax_rate = 0.045;
-tax = tax_rate + subtotal;
-
-/*Did on my own */
-if (subtotal < 100)
-     {shipping = 4}
-     else if (subtotal < 100)
-     {shipping = 7}
-     else if (subtotal > 100)
-     {shipping = 0.07*subtotal}
-
-     total = subtotal + tax + shipping; 
-/*Got table row code from Invoice WOD */
-     <tr>
-            <td width="43%">${item2}</td>
-            <td align="center" width="11%">${quantity2}</td>
-            <td width="13%">\$${price2}</td>
-            <td width="54%">\$${extended_price}</td>
-          </tr>
